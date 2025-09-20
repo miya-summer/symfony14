9a329863110ffc47dff14a16839c4f6fb99d1eb5
Shota Miyazaki <myzk.study@gmail.com>
2025-09-21 02:07:04 +0900

    フォームを$formだとレイアウトが微妙なので、templateを$form->renderで作り直してみる。
    [メモ]
    <?php use_stylesheet('form.css') ?>
    みたいな感じでtemplateに書いてあげると、headerでform.cssを読み込んだ状態で出力してくれる

--- apps/frontend/modules/user/templates/newSuccess.php
+++ apps/frontend/modules/user/templates/newSuccess.php
@@ -1,6 +1,22 @@
+<?php use_stylesheet('form.css') ?>
 <h1>ユーザー登録フォーム</h1>
 
 <form action="<?php echo url_for('user/create') ?>" method="post">
-    <?php echo $form ?>
-    <input type="submit" value="保存">
+    <?php echo $form->renderHiddenFields() ?>
+
+    <div class="form-group">
+        <?php echo $form['name']->renderLabel('名前 <span class="required">★</span>') ?>
+        <?php echo $form['name']->render(array('placeholder' => '名前を入力')) ?>
+        <?php echo $form['name']->renderError() ?>
+    </div>
+
+    <div class="form-group">
+        <?php echo $form['email']->renderLabel('Email <span class="required">★</span>') ?>
+        <?php echo $form['email']->render(array('placeholder' => 'example@example.com')) ?>
+        <?php echo $form['email']->renderError() ?>
+    </div>
+
+    <div class="form-actions">
+        <input type="submit" value="送信" />
+    </div>
 </form>
